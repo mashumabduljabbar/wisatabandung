bf7d6488b157141f15d9531eebbca4cec41fe284
Ma'shum Abdul Jabbar <multimediary@gmail.com>
2023-11-04 02:38:22 +0700

    Menampilkan gambar

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset('images/farm-house.jpg'),
+            Container(),
+            Container(),
+            Container(),
+            Image.network(
+    'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+          ],
+        ),
+      ),
+    );
+  }
+}
